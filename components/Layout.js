b123a2ad57f1aa072f0f492fcb1e32032c78fccd
vipgio <e.nabavi78@gmail.com>
2024-10-18 18:59:38 +0200

    home overflox x fix

--- components/Layout.js
+++ components/Layout.js
@@ -2,7 +2,7 @@ import Footer from "./Footer";
 import Header from "./Header";
 const Layout = ({ children }) => {
     return (
-        <div className="flex min-h-screen w-full flex-col justify-between bg-gradient bg-cover bg-center bg-no-repeat">
+        <div className="flex min-h-screen w-full flex-col justify-between overflow-x-hidden bg-gradient bg-cover bg-center bg-no-repeat">
             <Header />
             {children}
             <Footer />
